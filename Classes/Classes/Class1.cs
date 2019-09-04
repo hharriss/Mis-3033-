724c62ffbd9e74d1f1b9c60cc4283377c1e9517e
Harris Jones <hharrisjones@ou.edu>
2019-09-04 11:49:54 -0500

    Fr Participation for classes

---
+++ Classes/Classes/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Class1
+    {
+        public int CourseID { get; set; }
+        public int CourseNumber { get; set; }
+        public string CourseName { get; set; }
+        public string Subject { get; set; }
+        public string Instructor { get; set; }
+
+        public Course()
+        {
+
+        }
+        public override string ToString()
+        {
+            string msg = $"{CourseID}, {Subject}-{CourseNumber}, taught by {Instructor}";
+            //msg = CourseID + ", " + Subject + "-" + Course Number
+            return msg;
+        }
+    }
+}
